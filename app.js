08f218165f0b809d3725e1bf96e7f48598413846
rehman159 <rehman_uetpesh@yahoo.com>
2020-08-05 21:25:21 +0500

    logic is working pretty much till to that time

--- app.js
+++ app.js
@@ -13,4 +13,35 @@ var scores, roundScore, activePlayer, dice;
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
-dice = 6;
+dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector("#current-" + activePlayer).textContent = dice;
+document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  let dice = Math.floor(Math.random() * 6) + 1;
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+  if (dice !== 1) {
+    roundScore += dice;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    document.getElementById("current-" + activePlayer).textContent = "0";
+    activePlayer = activePlayer === 1 ? 0 : 1;
+    roundScore = 0;
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    document.querySelector(".dice").style.display = "none";
+  }
+});
+
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  scores[activePlayer] = scores[activePlayer] + roundScore;
+  document.querySelector("#score-" + activePlayer).textContent =
+    scores[activePlayer];
+});
