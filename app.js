ca96f2094981dbd1638fd770893cefdace596d9e
rehman159 <rehman_uetpesh@yahoo.com>
2020-08-06 22:43:51 +0500

    logic and working of the game is complete, need little bit more fine testing

--- app.js
+++ app.js
@@ -28,8 +28,18 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
   scores[activePlayer] = scores[activePlayer] + roundScore;
   document.querySelector("#score-" + activePlayer).textContent =
     scores[activePlayer];
-
-  nextPlayer();
+  if (scores[activePlayer] >= 20) {
+    document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+    document.querySelector(".dice").style.display = "none";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.add("winner");
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.remove("active");
+  } else {
+    nextPlayer();
+  }
 });
 
 function nextPlayer() {
